1cfef02f8a7309861d04dc3cc6e0298d1dda79ac
wilma242008 <136415157+wilma242008@users.noreply.github.com>
2022-12-24 18:27:05 -0600

    1.4.0 beta

---
+++ palera1n/ViewController.h
@@ -0,0 +1,27 @@
+//
+//  ViewController.h
+//  palera1n
+//
+//  Created by kristenlc on 12/18/22.
+//  Copyright © 2022 woofy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+@property (weak) IBOutlet NSTextField *statusTextField;
+
+@property (weak) IBOutlet NSButton *optionsButton;
+@property (weak) IBOutlet NSButton *startButton;
+
+@property NSArray* compatibleDevicesList;
+
+@property BOOL started;
+
+- (IBAction)optionsButton_DoClick:(id)sender;
+
+- (IBAction)startButton_DoClick:(id)sender;
+
+@end
+
